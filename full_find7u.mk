b5a6fbf6d59f5fcf7fb863d779e9e58b3a7ed66d
kejar31 <justin.a.rogers@gmail.com>
2014-08-28 15:44:01 +0200

    find7 to find7u
    Conflicts:
    	BoardConfig.mk
    	tg.mk

--- full_find7u.mk
+++ full_find7u.mk
@@ -16,11 +16,11 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit from find7 device
-$(call inherit-product, device/oppo/find7/find7.mk)
+$(call inherit-product, device/oppo/find7u/find7u.mk)
 
 # Discard inherited values and use our own instead.
-PRODUCT_NAME := full_find7
-PRODUCT_DEVICE := find7
+PRODUCT_NAME := full_find7u
+PRODUCT_DEVICE := find7u
 PRODUCT_BRAND := OPPO
 PRODUCT_MANUFACTURER := OPPO
 PRODUCT_MODEL := Find7
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,3 @@
-add_lunch_combo cm_find7-user
-add_lunch_combo cm_find7-userdebug
-add_lunch_combo cm_find7-eng
+add_lunch_combo cm_find7u-user
+add_lunch_combo cm_find7u-userdebug
+add_lunch_combo cm_find7u-eng
